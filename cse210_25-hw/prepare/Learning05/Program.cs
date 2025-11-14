a29243cbc490cbec3d31f571491be3e492fbc0a3
Your Name <lei21014@byui.edu>
2025-11-13 17:54:40 -0700

    Complete Learning05: Inheritance and Polymorphism with Shapes

--- cse210_25-hw/prepare/Learning05/Program.cs
+++ cse210_25-hw/prepare/Learning05/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Create a list of shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Add different shapes to the list
+        Square square = new Square("Red", 4);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Yellow", 5, 3);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Green", 6);
+        shapes.Add(circle);
+
+        // Iterate through the list and display color and area
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            
+            Console.WriteLine($"The {color} shape has an area of {area:F2}");
+        }
     }
 }
